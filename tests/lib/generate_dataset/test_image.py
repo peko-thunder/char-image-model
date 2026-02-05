249fa2f4cf25ef14746b0d8e3d4e3aa3a4a67f74
peko-thunder <eag000gle.0.3151993@gmail.com>
2026-02-05 22:12:19 +0900

    fix testコードの破壊

--- tests/lib/generate_dataset/test_image.py
+++ tests/lib/generate_dataset/test_image.py
@@ -1,35 +1,37 @@
 import os
 from PIL import Image
-from src.lib.generate_dataset.image import generate_font_char_image
+from src.lib.generate_dataset.image import generate_image
 
 
-def test_generate_font_char_image():
-    font_path = "fonts/ipamjm00601/ipamjm.ttf"
+def test_generate_image():
     char = "﨑"
-    output = "tests/dataset/test_output.jpg"
+    font_path = "fonts/ipamjm00601/ipamjm.ttf"
+    output_path = "tests/dataset/"
+    image_path = output_path + "FA11/ipamjm.jpg"
 
     try:
-        generate_font_char_image(font_path, char, output, image_size=256)
+        generate_image(char, font_path, output_path, image_size=256)
 
-        assert os.path.exists(output)
-        with Image.open(output) as img:
+        assert os.path.exists(image_path)
+        with Image.open(image_path) as img:
             assert img.size == (256, 256)
     finally:
-        if os.path.exists(output):
-            os.remove(output)
+        if os.path.exists(image_path):
+            os.remove(image_path)
 
 
-def test_generate_font_char_image_custom_size():
-    font_path = "fonts/ipamjm00601/ipamjm.ttf"
+def test_generate_image_custom_size():
     char = "﨑"
-    output = "tests/dataset/test_output_custom.jpg"
+    font_path = "fonts/ipamjm00601/ipamjm.ttf"
+    output_path = "tests/dataset/"
+    image_path = output_path + "FA11/ipamjm.jpg"
 
     try:
-        generate_font_char_image(font_path, char, output, image_size=512, font_size=400)
+        generate_image(char, font_path, output_path, image_size=512, font_size=400)
 
-        assert os.path.exists(output)
-        with Image.open(output) as img:
+        assert os.path.exists(image_path)
+        with Image.open(image_path) as img:
             assert img.size == (512, 512)
     finally:
-        if os.path.exists(output):
-            os.remove(output)
+        if os.path.exists(image_path):
+            os.remove(image_path)
