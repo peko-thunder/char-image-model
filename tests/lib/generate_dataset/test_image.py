78e2c69bcba676185cc2c6eb7c58cf67b4643a97
peko-thunder <eag000gle.0.3151993@gmail.com>
2026-02-07 10:54:36 +0900

    change テスト画像が消えないように調整

--- tests/lib/generate_dataset/test_image.py
+++ tests/lib/generate_dataset/test_image.py
@@ -6,32 +6,26 @@ from src.lib.generate_dataset.image import generate_image
 def test_generate_image():
     char = "﨑"
     font_path = "fonts/ipamjm00601/ipamjm.ttf"
-    output_path = "tests/dataset/"
-    image_path = output_path + "FA11/ipamjm.jpg"
+    output_path = "tests/dataset/generate_image"
+    unicode_hex = format(ord(char), "04X")
+    image_path = f"{output_path}/{unicode_hex}/ipamjm.jpg"
 
-    try:
-        generate_image(char, font_path, output_path, image_size=256)
+    generate_image(char, font_path, output_path, image_size=256)
 
-        assert os.path.exists(image_path)
-        with Image.open(image_path) as img:
-            assert img.size == (256, 256)
-    finally:
-        if os.path.exists(image_path):
-            os.remove(image_path)
+    assert os.path.exists(image_path)
+    with Image.open(image_path) as img:
+        assert img.size == (256, 256)
 
 
 def test_generate_image_custom_size():
     char = "﨑"
     font_path = "fonts/ipamjm00601/ipamjm.ttf"
-    output_path = "tests/dataset/"
-    image_path = output_path + "FA11/ipamjm.jpg"
+    output_path = "tests/dataset/generate_image_custom_size"
+    unicode_hex = format(ord(char), "04X")
+    image_path = f"{output_path}/{unicode_hex}/ipamjm.jpg"
 
-    try:
-        generate_image(char, font_path, output_path, image_size=512, font_size=400)
+    generate_image(char, font_path, output_path, image_size=512, font_size=400)
 
-        assert os.path.exists(image_path)
-        with Image.open(image_path) as img:
-            assert img.size == (512, 512)
-    finally:
-        if os.path.exists(image_path):
-            os.remove(image_path)
+    assert os.path.exists(image_path)
+    with Image.open(image_path) as img:
+        assert img.size == (512, 512)
